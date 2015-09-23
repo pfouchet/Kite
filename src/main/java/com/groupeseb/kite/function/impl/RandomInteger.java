2e7df35e57ab43494f87f18c0b051e2e3a125615
pfouchet <pfouchet.extern@groupeseb.com>
2015-09-23 14:18:29 +0200

    Add RandomInteger method

---
+++ src/main/java/com/groupeseb/kite/function/impl/RandomInteger.java
@@ -0,0 +1,25 @@
+package com.groupeseb.kite.function.impl;
+
+import com.groupeseb.kite.CreationLog;
+import com.groupeseb.kite.function.Function;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class RandomInteger extends Function {
+    private final Random randomGenerator = new Random();
+
+    public String getName() {
+        return "RandomInteger";
+    }
+
+    public String apply(List<String> parameters, CreationLog creationLog) {
+        return Integer.toString(randomGenerator.nextInt());
+    }
+}
