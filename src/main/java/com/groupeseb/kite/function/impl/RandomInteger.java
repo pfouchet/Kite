f9ef2ff0d14f430a9ffdea532313821efc36aea5
vdealmeida <u9o$KEHJcW518AB>
2015-10-06 10:57:22 +0200

    Random function with parameter

--- src/main/java/com/groupeseb/kite/function/impl/RandomInteger.java
+++ src/main/java/com/groupeseb/kite/function/impl/RandomInteger.java
@@ -4,22 +4,25 @@ import com.groupeseb.kite.CreationLog;
 import com.groupeseb.kite.function.Function;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Random;
-import java.util.UUID;
 
 @Slf4j
 @Component
 public class RandomInteger extends Function {
-    private final Random randomGenerator = new Random();
+	private final Random randomGenerator = new Random();
 
-    public String getName() {
-        return "RandomInteger";
-    }
+	public String getName() {
+		return "RandomInteger";
+	}
 
-    public String apply(List<String> parameters, CreationLog creationLog) {
-        return Integer.toString(randomGenerator.nextInt());
-    }
+	public String apply(List<String> parameters, CreationLog creationLog) {
+		if (CollectionUtils.isEmpty(parameters)) {
+			return Integer.toString(randomGenerator.nextInt());
+		} else {
+			return Integer.toString(randomGenerator.nextInt(Integer.parseInt(parameters.get(0))));
+		}
+	}
 }
