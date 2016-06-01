fdd5ea5beb9987ad86f8f8d0538ce68ed8ebe55a
pfouchet <pfouchet.extern@groupeseb.com>
2016-06-01 09:36:53 +0200

    Refactoring

--- src/main/java/com/groupeseb/kite/check/Check.java
+++ src/main/java/com/groupeseb/kite/check/Check.java
@@ -27,10 +27,10 @@ public class Check {
 			log.warn("'description' field is missing in one of your check.");
 		}
 
-		description = (checkSpecification.getString("description") == null) ? "" : checkSpecification.getString("description");
-		fieldName = (checkSpecification.getString("field") == null) ? null : context.processPlaceholdersInString(checkSpecification.getString("field"));
-		methodName = (checkSpecification.getString("method") == null) ? "nop" : checkSpecification.getString("method");
-		operatorName = (checkSpecification.getString("operator") == null) ? "equals" : checkSpecification.getString("operator");
+		description = checkSpecification.getStringOrDefault("description", "");
+		fieldName = context.processPlaceholdersInString(checkSpecification.getString("field"));
+		methodName = checkSpecification.getStringOrDefault("method", "nop");
+		operatorName = checkSpecification.getStringOrDefault("operator", "equals");
 		expectedValue = context.processPlaceholders(checkSpecification.getObject("expected"));
 		parameters = checkSpecification.get("parameters");
 		foreach = checkSpecification.getBooleanOrDefault("foreach", false);
