6d97c5ce00969da7eaa4c2c81f20972739ab825b
bdjelaili <db@solbe-soft.fr>
2016-08-03 10:06:20 +0200

    PR feedback

--- src/main/java/com/groupeseb/kite/FileHelper.java
+++ src/main/java/com/groupeseb/kite/FileHelper.java
@@ -11,6 +11,12 @@ import java.io.InputStream;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class FileHelper {
 
+	/**
+	 * Create a new InputStream for input fileName.
+	 * The resource must be exist in the Classpath
+	 *
+	 * @param filename the absolute path within the class path
+	 */
 	public static InputStream getFileInputStream(String filename) {
 		ClassPathResource resource = new ClassPathResource(filename);
 
