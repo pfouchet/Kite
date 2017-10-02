5389f76ea539a3c98e03c8b2f28a40b497bc32c2
Guillaume Darona <gdarona.extern@groupeseb.com>
2017-10-02 10:55:42 +0200

    #49760 - change constant String to ContentType

--- src/main/java/com/groupeseb/kite/CommandRunner.java
+++ src/main/java/com/groupeseb/kite/CommandRunner.java
@@ -10,7 +10,6 @@ import junit.framework.Assert;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.entity.ContentType;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpParams;
 import org.json.simple.parser.ParseException;
@@ -34,8 +33,7 @@ import static org.testng.AssertJUnit.assertEquals;
 @Slf4j
 @Component
 public class CommandRunner {
-	private static final ContentType APPLICATION_JSON_UTF8 = ContentType.create(
-			APPLICATION_JSON.getMimeType(), "UTF-8");
+
 	private static final String POST = "POST";
 	private static final String PUT = "PUT";
 	private static final String DELETE = "DELETE";
@@ -92,7 +90,7 @@ public class CommandRunner {
 		log.info("[ {} ] POST {} (expecting {})", command.getName(), processedURI, command.getExpectedStatus());
 
 		Response postResponse = contextProcessor.initRequestSpecificationContent(command)
-				.contentType(APPLICATION_JSON_UTF8.toString())
+				.contentType(APPLICATION_JSON.toString())
 				.headers(contextProcessor.getProcessedHeaders(command))
 				.when()
 				.post(processedURI);
@@ -120,7 +118,7 @@ public class CommandRunner {
 		}
 
 		log.info("Checking resource: " + location + "...");
-		given().header("Accept-Encoding", APPLICATION_JSON_UTF8.getCharset().toString())
+		given().header("Accept-Encoding", APPLICATION_JSON.getCharset().toString())
 				.headers(contextProcessor.getProcessedHeadersForCheck(command))
 				.expect().statusCode(HttpStatus.SC_OK)
 				.when().get(location);
@@ -136,7 +134,7 @@ public class CommandRunner {
 		log.info("[{}] PATCH {} (expecting {})", command.getName(), processedURI, command.getExpectedStatus());
 
 		Response patchResponse = contextProcessor.initRequestSpecificationContent(command)
-				.contentType(APPLICATION_JSON_UTF8.toString())
+				.contentType(APPLICATION_JSON.toString())
 				.headers(contextProcessor.getProcessedHeaders(command))
 				.when()
 				.patch(processedURI);
@@ -167,7 +165,7 @@ public class CommandRunner {
 			mapHeaders.put(header.getKey(), header.getValue());
 		}
 
-		Response response = given().contentType(APPLICATION_JSON_UTF8.toString())
+		Response response = given().contentType(APPLICATION_JSON.toString())
 				.headers(mapHeaders)
 				.expect().statusCode(command.getExpectedStatus())
 				.when().get(processedURI);
@@ -222,7 +220,7 @@ public class CommandRunner {
 		log.info("[ {} ] PUT {} (expecting {})", command.getName(), processedURI, command.getExpectedStatus());
 
 		Response putResponse = contextProcessor.initRequestSpecificationContent(command)
-				.contentType(APPLICATION_JSON_UTF8.toString())
+				.contentType(APPLICATION_JSON.toString())
 				.headers(contextProcessor.getProcessedHeaders(command))
 				.log()
 				.everything(true)
@@ -246,7 +244,7 @@ public class CommandRunner {
 		log.info("DELETE " + processedURI + " (expecting " + expectedStatus + ')');
 
 		Response r = contextProcessor.initRequestSpecificationContent(command)
-				.contentType(APPLICATION_JSON_UTF8.toString())
+				.contentType(APPLICATION_JSON.toString())
 				.headers(contextProcessor.getProcessedHeaders(command))
 				.log()
 				.everything(true)
@@ -260,7 +258,7 @@ public class CommandRunner {
 		log.info("Checking resource: " + processedURI + "...");
 
 		if (command.getAutomaticCheck()) {
-			given().contentType(APPLICATION_JSON_UTF8.toString())
+			given().contentType(APPLICATION_JSON.toString())
 					.expect().statusCode(HttpStatus.SC_NOT_FOUND)
 					.when().get(processedURI);
 		}
